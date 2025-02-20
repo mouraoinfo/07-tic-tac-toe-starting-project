24b9eb2239a1c0f060743489e267c007589833f4
Marcos Mourão <dev@mourao.info>
2025-02-20 16:03:47 -0300

    add player component and update app structure with game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import Player from "./components/Player"
 
 function App() {
   
 
   return (
-    <>
-      <p>Coming soon...</p>
-    </>
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <Player name="Player 1" symbol="X" />
+            <Player name="Player 2" symbol="O" />
+          </ol>
+          <div id="game-board">
+
+          </div>
+           GAME BOARD
+        </div>
+
+        LOG
+      </main>
   )
 }
 
