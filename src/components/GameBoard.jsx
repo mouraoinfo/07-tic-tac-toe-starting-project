b5d027963ff12a0c25317e85e827f1aae25f8041
Marcos Mourão <dev@mourao.info>
2025-03-08 04:40:30 -0300

    refactor GameBoard component to use a new game board structure and update rendering logic

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,47 +2,39 @@ import { useState } from "react";
 
 
 const inicialGameBoard = [
-[null, null, null],
-[null, null, null],
-[null, null, null] 
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9] 
 ];
+
+
+const meuArray = [11, 22, 33];
+const segundoArray = [44, 55, 66];
+const terceiroArray = [77, 88, 99];
+
+const composto = [meuArray, segundoArray, terceiroArray];
 
 
 export default function GameBoard() {
 
     const [gameBoard, setGameBoard] = useState(inicialGameBoard);
 
-    function handleSelectSquare (rowIndex, cellIndex) {
-        setGameBoard((prevGameBoard) => {
-            const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updateBoard[rowIndex][cellIndex]  = 'x';
-            return updateBoard;
-        }
-
-        );
-    }
-
+  
 
 
   return (
-    <ol id="game-board">
-        {gameBoard.map((row, rowIndex) => (
-            
-            <li key={rowIndex}>
-           
-                <ol>
-                    {row.map((cell, cellIndex) => (
-                        <li key={cellIndex} className="cell">
-                            <button className="cell-button" onClick={() =>handleSelectSquare(rowIndex, cellIndex)}>
-                                {cell}
-                            </button>
-                        </li>
-      
-                    ))}
-                </ol>
-            </li>
-        ))}
+  
 
+    <ol>
+        {composto.map( 
+                    (row, rowIndex) => 
+                        <li key={rowIndex}>
+                            <ol>
+                                {row.map((item) => <li> <button>{item}</button> </li>) }
+                            </ol>
+                        </li> 
+          )
+        }
     </ol>
 );
 }
