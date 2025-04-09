61c76056f8fa64c8d9d16942622e7fa4db53a703
Marcos Mourão <dev@mourao.info>
2025-04-09 18:02:26 -0300

    update index.html title to reflect new step 87 and refactor active player logic in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,34 @@ import GameBoard from "./components/GameBoard"
 
 import Log from "./components/Log"
 
+function deriveActivePlayer(gameTurns) {
+  
+  let currentPlayer = 'X';
+
+  if (gameTurns.length > 0 &&  gameTurns[0].player === 'X') {
+    currentPlayer="O";
+  }
+
+  return currentPlayer;
+
+}
 
 
 function App() {
 
-  // o step 84 não teve avanços significativos, apenas explicação de conceitos
+  // 
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, SetActivePlayer] = useState('X');
+  // const [activePlayer, SetActivePlayer] = useState('X');
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+
+
 
   function handleSelectSquare (rowIndex, colIndex) {
-    SetActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  // SetActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns( prevTurns => {
 
-      let currentPlayer = 'X';
-
-      if (prevTurns.length > 0 &&  prevTurns[0].player === 'X') {
-        currentPlayer="O";
-      }
+    const currentPlayer = deriveActivePlayer(prevTurns);
 
       const updatedTurns = [ {square: {row: rowIndex, col: colIndex}, player: activePlayer} , ...prevTurns];
 
