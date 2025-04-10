2c850559fd50acd03d2eb1134c47068777589b6a
Marcos Mourão <dev@mourao.info>
2025-04-10 01:32:33 -0300

    update index.html title to reflect new step 88 and disable buttons for already selected squares in GameBoard component

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -43,7 +43,10 @@ export default function GameBoard( { onSelectSquare, turns } ) {
                 <ol>
                     {row.map((playerSymbol, colIndex) => (
                         <li key={colIndex}>
-                            <button onClick={ () => onSelectSquare(rowIndex, colIndex) } >
+                            <button 
+                                onClick={ () => onSelectSquare(rowIndex, colIndex) }
+                                disabled = {playerSymbol != null}
+                            >
                                 {playerSymbol}
                             </button>
                         </li>
