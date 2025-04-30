e2a01b214c5ac0c2a2c4b18810b251f8204c7e95
mouraoinfo <dev@mourao.info>
2025-04-30 19:44:50 -0300

    feat: add Footer component and update styles for improved layout and responsiveness

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import '../styles/Footer.css';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <ul className="footer-content">
+                <li>React Practice Tic-Tac-Toe</li>
+                <li className="version">Beta 1.0</li>
+                <li>
+                    <a 
+                        href="https://mourao.info" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        Created by mourao.info
+                    </a>
+                </li>
+            </ul>
+        </footer>
+    );
+}
+
+export default Footer;
