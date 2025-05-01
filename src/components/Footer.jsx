30c6189901a496f9f0f6c7d5bb1b8312efafda51
mouraoinfo <dev@mourao.info>
2025-05-01 00:52:49 -0300

    fix: update footer version label to Public 1.1 and enhance button styles for responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
         <footer className="footer">
             <ul className="footer-content">
                 <li>React Practice Tic-Tac-Toe</li>
-                <li className="version">Version Public  1.0</li>
+                <li className="version">Version Public  1.1</li>
                 <li>
                     <a 
                         href="https://mourao.info" 
